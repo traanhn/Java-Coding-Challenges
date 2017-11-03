148e43736b1835e3dfbc80ea68d5c58135b8c1b1
Tra-Anh Nguyen <traanh92@gmail.com>
2017-11-03 12:42:13 +0200

    Add files via upload

---
+++ week1-003.Spruce/src/Spruce.java
@@ -0,0 +1,18 @@
+public class Spruce {
+
+    public static void main(String[] args) {
+
+        // Write code here
+        
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+        
+        
+    }
+    
+
+}
