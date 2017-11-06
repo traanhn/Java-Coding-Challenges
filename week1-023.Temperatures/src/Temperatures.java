60668265a62278ca2aadb045f19a5eaacb20162b
Tra-Anh Nguyen <traanh92@gmail.com>
2017-11-06 16:52:55 +0200

    Add files via upload

---
+++ week1-023.Temperatures/src/Temperatures.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Temperatures {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        // Write your code here. 
+        while (true) {
+            System.out.print("Enter the value : ");
+            double temperature = Double.parseDouble(reader.nextLine());
+            if (temperature >= -30 && temperature <= 40 ) {
+            Graph.addNumber(temperature);
+            
+            } else {
+                System.out.println("No Valid"); 
+            
+            } 
+            
+        
+        
+    }
+    }
+}
